d9f223c6d303a8959586238c5511cdd5bc8e6fe6
Kh4wad <khawad@minha.fag.edu.br>
2026-06-04 16:03:55 -0300

    Cards de precos criados

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Box from "../assets/box.svg";
 import Caminhao from "../assets/Caminhao.svg";
+import Check from "../assets/Check.png";
 import Close from "../assets/close.svg";
 import Escudo from "../assets/Escudo.svg";
 import Facebook from "../assets/facebook 1.png";
@@ -17,6 +18,7 @@ import "../styles/Entrarcontato.css";
 import "../styles/Footer.css";
 import "../styles/header.css";
 import "../styles/hero.css";
+import "../styles/Pricing.css";
 import "../styles/solution.css";
 import "../styles/testimonials.css";
 import "../styles/utility.css";
@@ -163,23 +165,17 @@ export default function Home() {
         <div className="container content">
           <p className="desktop-only">Seja Bem Vindo</p>
           <h1>
-            Cansou de ser roubado, entao hoje e o seu dia de sorte, estamos aqui
-            para te ajudar!
+            Cansou de ser roubado? Então, hoje é o seu dia de sorte, estamos aqui para te ajudar!
           </h1>
           <p>
-            Ja pensou em evitar perder sua encomenda, ou ate mesmo evitar ser
-            roubado? Com a nossa tecnologia de ponta, voce pode ficar tranquilo
-            sabendo que suas encomendas estao seguras com os nossos
-            dispositivos. Nao perca mais tempo se preocupando com o roubo de
-            encomendas, junte-se a nos hoje e experimente a diferenca que a
-            nossa seguranca pode fazer!
+            Já pensou em evitar perder sua encomenda, ou até mesmo evitar ser roubado? Com a nossa tecnologia de ponta, você pode ficar tranquilo sabendo que suas encomendas estão seguras com os nossos dispositivos. Não perca mais tempo se preocupando com o roubo de encomendas, junte-se a nós hoje e experimente a diferença que a nossa segurança pode fazer!
           </p>
           <div className="flex gap-1">
             <span>
               <Button text="Cadastre-se" />
             </span>
             <span className="desktop-only">
-              <Button text="Para mais informacoes" secondary />
+              <Button text="Saiba mais" secondary />
             </span>
           </div>
         </div>
@@ -194,21 +190,13 @@ export default function Home() {
             </span>
           </span>
           <p>
-            Inovação é com a gente! A <strong>DonaFrost </strong>
-            já conquistou diversos clientes, seja você mais um deles, veja tudo
-            que pode ganhar com nossos serviços.
+            Inovação é o nosso forte! A <strong>Securitybox</strong> já conquistou a confiança de milhares de clientes. Venha ser o próximo e descubra tudo o que podemos fazer para a proteção de suas encomendas.
           </p>
         </header>
         <section className="even-columns">
-          <Card img={Box} subtitulo="Produto Vencedor" descricao="Ideia matadora, nosso time já ganhou diversos eventos de
-                inovação com nosso produto, entre eles podemos citar o CityFarm
-                da FAG e Startup Garage." />
-          <Card img={Caminhao} subtitulo="Produto Vencedor" descricao="Ideia matadora, nosso time já ganhou diversos eventos de
-                inovação com nosso produto, entre eles podemos citar o CityFarm
-                da FAG e Startup Garage." />
-          <Card img={Escudo} subtitulo="Produto Vencedor" descricao="Ideia matadora, nosso time já ganhou diversos eventos de
-                inovação com nosso produto, entre eles podemos citar o CityFarm
-                da FAG e Startup Garage." />
+          <Card img={Box} subtitulo="Caixas seguras" descricao="Usamos rastreador de encomendas, analise de identidade por toque e alerta de roubo a autoridades por dispositivos embutidos ou aplicativos." />
+          <Card img={Caminhao} subtitulo="Entregas mais seguras" descricao="Com a nossa tecnologia podemos diminuir os casos de entregas em lugares incorretos, garantindo que suas encomendas cheguem onde devem estar." />
+          <Card img={Escudo} subtitulo="Embalagens e segurança" descricao="Nossas embalagens são projetadas para garantir a integridade e segurança de seus produtos durante o transporte e à prova de água." />
         </section>
       </section>
       {/* Continuar daqui */}
@@ -223,8 +211,8 @@ export default function Home() {
             <h2>Cada cliente importa!</h2>
           </span>
           <p>
-            Quem já pediu sabe da qualidade das nossas receitas, estamos tirando aquela ideia de que
-            comida congelada tem de ser algo sem gosto, acompanhe abaixo os testemunhos de quem já comprou e aprovou.
+            Quem nos conhece sabe do quanto somos seguros com suas encomendas, estamos tirando aquela ideia de que
+            seu produto nunca esta seguro, veja abaixo alguns de nossos clientes satisfeitos.
           </p>
 
         </header>
@@ -314,8 +302,8 @@ export default function Home() {
           </span>
           <p className="textolongo">
             Entre em contato, estamos dispostos a tirar qualquer dúvida,
-            seja um orçamento, uma dúvida técnica de algum de nossos produtos.
-            Estamos à disposição para responder.😎
+            seja um orçamento, uma dúvida técnica de como nossos produtos podem beneficia-lo.
+            Estamos à disposição para responder. 😊
           </p>
           <p className="textocurto">Entre em contato, estamos dispostos a <br /> tirar qualquer dúvida😎.</p>
         </header>
@@ -331,10 +319,95 @@ export default function Home() {
 
       </section>
 
+      {/* Planos */}
+
+      <section id="pricing" className="container">
+        <header>
+          <p className="desktop-only">
+            Planos e preços
+          </p>
+          <h2>Nossos planos</h2>
+        </header>
+        <section className="even-columns gap-1.5">
+          {/* Plano Básico */}
+          <div className="pricing-card">
+            <span className="plan">
+              <h3>Básico</h3>
+              <p>Você tem direito a usar nosso aplicativo de localizador de encomendas</p>
+            </span>
+            <h2>Grátis</h2>
+            <Button text="Abaixe aqui" secondary key="free" />
+            <span className="hr" />
+            <span className="features">
+              <img src={Check} alt="ícone check" width={24} height={24} />
+              <p>Evite ser roubado</p>
+            </span> <ul className="features">
+              <li> <img src={Check} alt="ícone check" width={24} height={24} />
+                <p>Seguro</p>
+              </li>
+            </ul>
+          </div>
+          {/* Plano Premium */}
+          <div className="pricing-card premium">
+            <span className="bonus">
+              <p>1º MÊS COM DESCONTO</p>
+            </span>
+            <span className="plan">
+              <h3>Premium</h3>
+              <p>
+                Analise de reconhecimento por digital, rastreamento de encomendas e alerta de roubo a autoridades por dispositivos embutidos ou aplicativos.
+              </p>
+            </span>
+            <span className="price">
+              <h2>R$ 60,00</h2>
+              <p>/mês</p>
+            </span>
+            <Button text="Pedir agora" key="premium" />
+            <span className="hr" />
+            <span className="features">
+              <img src={Check} alt="ícone check" width={24} height={24} />
+              <p>Analise de reconhecimento por digital na embalagem</p>
+            </span>
+            <span className="features">
+              <img src={Check} alt="ícone check" width={24} height={24} />
+              <p>rastreamento de encomendas</p>
+            </span>
+            <span className="features">
+              <img src={Check} alt="ícone check" width={24} height={24} />
+              <p>alerta de roubo a autoridades</p>
+            </span>
+          </div>
+
+           {/* Plano Medio */}
+           <div className="pricing-card">
+            <span className="plan">
+              <h3>Medio</h3>
+              <p>Você tem direito a usar nosso aplicativo de localizador de encomendas e reconhecimento por digital</p>
+            </span>
+            <span className="price">
+            <h2>R$ 30,00</h2>
+            <p>/mês</p>
+            </span>
+            <Button text="Pedir agora" secondary key="free" />
+            <span className="hr" />
+            <span className="features">
+              <img src={Check} alt="ícone check" width={24} height={24} />
+              <p>Aumenta a segurança</p>
+            </span> <ul className="features">
+              <li> <img src={Check} alt="ícone check" width={24} height={24} />
+                <p>Evite perder seus produtos</p>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </section>
+
+
+      {/* Planos */}
       <footer id="footer">
         <section className="Redesociais">
           <div className="logomarca">
-            <h2>LogoMarca</h2>
+            <img src="src/assets/logo.svg" alt="Logo securitybox" />
             <div>
               <img src={Instagram} alt="Instagram" />
               <img src={Facebook} alt="Facebook" />
@@ -372,7 +445,7 @@ export default function Home() {
 
         </section>
         <section className="Copyright">
-          <p>Feito com amor na aula de Programação Web💙 ©2026 SecurityBox - Todos os direitos reservados.</p>
+          <p>Feito com amor na aula de Programação Web❤️ ©2026 SecurityBox - Todos os direitos reservados.</p>
         </section>
       </footer>
 
